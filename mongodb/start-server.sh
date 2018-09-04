6a02efc7bc3df8b07573307b993dfb7901e30c8c
Michael Mior <mmior@cs.rit.edu>
2018-09-04 15:46:21 -0400

    Properly convert ObjectIds

--- mongodb/start-server.sh
+++ mongodb/start-server.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 echo 'db.country.insert([' > country.js
-curl -s 'https://raw.githubusercontent.com/ozlerhakan/mongodb-json-files/master/datasets/country.json' | sed 's/$/,/' >> country.js
+curl -s 'https://raw.githubusercontent.com/ozlerhakan/mongodb-json-files/master/datasets/country.json' | sed 's/{"$oid":\("[^"]*"\)}/ObjectId(\1)/;s/$/,/' >> country.js
 echo '])' >> country.js
 docker run --detach --name=mongo --volume $(pwd):/host mvertes/alpine-mongo:4.0.1-0
