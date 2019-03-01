ee32c1acfacc03cceefad44878f5d748f08010fd
Michael Mior <mmior@cs.rit.edu>
2019-03-01 17:53:47 -0500

    Fix population data

--- mongodb/start-client.sh
+++ mongodb/start-client.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 clear
 until (clear && echo 'Please wait...' && cat data.js | docker exec -i mongo mongo --quiet) 2> /dev/null; do sleep 5; done
+cat population.json | docker exec -i mongo mongoimport --jsonArray -c pop
+clear
 docker exec -it mongo mongo --quiet
--- mongodb/start-server.sh
+++ mongodb/start-server.sh
@@ -2,7 +2,5 @@
 echo 'db.country.insert([' > data.js
 curl -s 'https://raw.githubusercontent.com/ozlerhakan/mongodb-json-files/master/datasets/country.json' | sed 's/{"$oid":\("[^"]*"\)}/ObjectId(\1)/;s/$/,/' >> data.js
 echo '])' >> data.js
-# echo 'db.pop.insert([' >> data.js
-# curl  -s https://pkgstore.datahub.io/core/population/population_json/data/43d34c2353cbd16a0aa8cadfb193af05/population_json.json | sed 's/^\[//;s/\]$//' >> data.js
-# echo '])' >> data.js
+curl -s https://pkgstore.datahub.io/core/population/population_json/data/43d34c2353cbd16a0aa8cadfb193af05/population_json.json > population.json
 docker run --detach --name=mongo --volume $(pwd):/host mvertes/alpine-mongo:4.0.1-0
